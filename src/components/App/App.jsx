f944d85147b938b466ee70b0cfaca31cdfe39d9f
TanyaKuchak <tanyakuchak@ukr.net>
2022-12-13 18:22:34 +0200

    “”

---
+++ src/components/App/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Component } from 'react';
+import { Container } from './App.styled';
+// import ContactForm from 'components/ContactForm/ContactForm';
+class App extends Component {
+  state = {
+    inputValue: 'qwe',
+  };
+  handleInputChange = e => {
+    console.log(e);
+  };
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+  render() {
+    return (
+      <Container>
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+        />
+      </Container>
+    );
+  }
+}
+export default App;
