0f847262f9f332dd0bf198ed215b1d3bed895ebe
TanyaKuchak <tanyakuchak@ukr.net>
2022-12-19 15:08:00 +0200

    add formik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,67 +1,86 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { Formik } from 'formik';
+// import { Component } from 'react';
 import { Form, Label, Input, Button } from './ContactForm.styled';
 
-class ContactForm extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-  state = {
+const ContactForm = ({ contacts, onFormSubmit }) => {
+  const initialValues = {
     name: '',
-    number: '',
+    namber: '',
+  };
+  const handleSubmit = (values, { resetForm }) => {
+    resetForm();
+    onFormSubmit(values);
   };
 
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
+  // class ContactForm extends Component {
+  //   static propTypes = {
+  //     onSubmit: PropTypes.func.isRequired,
+  //   };
+  //   state = {
+  //     name: '',
+  //     number: '',
+  //   };
 
-    this.setState({
-      [name]: value,
-    });
-  };
+  //   handleChange = e => {
+  //     const { name, value } = e.currentTarget;
 
-  handleSubmit = e => {
-    e.preventDefault();
+  //     this.setState({
+  //       [name]: value,
+  //     });
+  //   };
 
-    const { name, number } = this.state;
+  //   // handleSubmit = e => {
+  //   //   e.preventDefault();
 
-    if (this.props.onSubmit(name, number)) {
-      this.setState({ name: '', number: '' });
-    }
-  };
+  //   //   const { name, number } = this.state;
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Label>
+  //   //   if (this.props.onSubmit(name, number)) {
+  //   //     this.setState({ name: '', number: '' });
+  //   //   }
+  //   // };
+
+  //   render() {
+  //     const { name, number } = this.state;
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form autoComplete="of">
+        <Label htmlFor="name">
           Name
           <Input
             type="text"
             name="name"
-            value={name}
             placeholder="Franko Ivan"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            onChange={this.handleChange}
           />
         </Label>
-        <Label>
+        <Label htmlFor="number">
           Number
           <Input
             type="tel"
             name="number"
-            value={number}
             placeholder="111-11-111"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
-            onChange={this.handleChange}
           />
         </Label>
         <Button type="submit">Add contact</Button>
       </Form>
-    );
-  }
-}
+    </Formik>
+  );
+};
+
+ContactForm.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onFormSubmit: PropTypes.func.isRequired,
+};
 export default ContactForm;
